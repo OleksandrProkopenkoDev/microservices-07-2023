744f7408974115d2f26a685eee841775fff0f13a
SPro <prokopenko89@gmail.com>
2023-08-01 21:03:32 +0300

    discovery server finished

--- order-service/src/main/java/com/spro/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/spro/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -38,9 +38,9 @@ public class OrderService {
                 .toList();
 
 //        call Inventory Service, and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
                 .uri(
-                        "http://localhost:8082/api/inventory",
+                        "http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
                         )
                 .retrieve()
